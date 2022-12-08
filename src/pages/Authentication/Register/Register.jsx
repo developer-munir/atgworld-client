d4e1049db4685d03369e0a81e9345e2edd444f86
Munir Hossain Juwel <munirhossainjuwel@gmail.com>
2022-12-08 17:58:20 +0600

    setup login modal

--- src/pages/Authentication/Register/Register.jsx
+++ src/pages/Authentication/Register/Register.jsx
@@ -5,6 +5,7 @@ import registerBanner from "../../../assets/513049 PI8X2J 876.png";
 const Register = () => {
   return (
     <div>
+      {/* register modal */}
       <div
         class="modal fade"
         id="exampleModal"
@@ -38,13 +39,13 @@ const Register = () => {
             </div>
 
             <div class="modal-body ">
-              <div className="row">
+              <div className="row p-3">
                 <div className="col-lg-6">
                   <div className="d-flex justify-content-between align-items-center">
                     <h1 className="fw-bold fs-3 mb-3">Create Account</h1>
                     <button
                       type="button"
-                      class="btn-close "
+                      class="btn-close d-lg-none"
                       data-bs-dismiss="modal"
                       aria-label="Close"
                     ></button>
@@ -54,7 +55,80 @@ const Register = () => {
                 <div className="col-6 d-none d-lg-block">
                   <p className="text-end mb-3">
                     Already have an account?
-                    <button className="singnInBtn">Sign In</button>
+                    <button
+                      className="singnInBtn"
+                      data-bs-target="#exampleModalToggle2"
+                      data-bs-toggle="modal"
+                      data-bs-dismiss="modal"
+                    >
+                      Sign In
+                    </button>
+                  </p>
+                  <div>
+                    <img src={registerBanner} alt="" />
+                    <p className="privacy-text">
+                      By signing up, you agree to our Terms & conditions,
+                      Privacy policy
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* login modal */}
+
+      <div
+        class="modal fade"
+        id="exampleModalToggle2"
+        aria-hidden="true"
+        aria-labelledby="exampleModalToggleLabel2"
+        tabindex="-1"
+      >
+        <button
+          type="button"
+          class="btn-close close-btn-modal d-none d-lg-block"
+          data-bs-dismiss="modal"
+          aria-label="Close"
+        ></button>
+        <div class="modal-dialog  modal-dialog-centered">
+          <div class="modal-content modal-sizing">
+            <div class="modal-header  border-0">
+              <span
+                class="modal-title modal-top-title modal-header"
+                id="exampleModalLabel"
+              >
+                Let's learn, share & inspire each other with our passion for
+                computer engineering. Sign up now 🤘🏼
+              </span>
+
+              <button
+                type="button"
+                class="btn-close d-lg-none"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+
+            <div class="modal-body ">
+              <div className="row p-3">
+                <div className="col-lg-6">
+                  <div className="d-flex justify-content-between align-items-center">
+                    <h1 className="fw-bold fs-3 mb-3">Sign In </h1>
+                    <button
+                      type="button"
+                      class="btn-close d-lg-none"
+                      data-bs-dismiss="modal"
+                      aria-label="Close"
+                    ></button>
+                  </div>
+                  <RegisterField></RegisterField>
+                </div>
+                <div className="col-6 d-none d-lg-block">
+                  <p className=" mb-3">
+                    Don’t have an account yet? Create new for free!
                   </p>
                   <div>
                     <img src={registerBanner} alt="" />
